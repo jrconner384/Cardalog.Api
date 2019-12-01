dab42daf4ef6c254cfc70243bdc3abcf4fcda08b
Jason Conner <jason.r.conner@gmail.com>
2019-11-30 20:26:25 -0600

    First refactor from the scaffolded Function. I haven't tested this yet.

--- WriteCard.cs
+++ WriteCard.cs
@@ -1,16 +1,18 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.IO;
 using Newtonsoft.Json;
 
 namespace Cardalog.Api
 {
-    public static class WriteCard
+  public static class WriteCard
     {
         // See ./Seeds/mtg-cards.json for an example of the expected JSON
         [FunctionName("WriteCard")]
@@ -18,17 +20,56 @@ namespace Cardalog.Api
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = "cards")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                var client = new MongoClient("mongodb://127.0.0.1:27017");
+                var db = client.GetDatabase("cardalog");
+                var coll = db.GetCollection<BsonDocument>("cards");
+                var body = await new StreamReader(req.Body).ReadToEndAsync();
+                // Seems quickest to start with a dynamic. Might want to look into a data types library to share amongst the UI and Functions like this.
+                dynamic data = JsonConvert.DeserializeObject(body);
 
-            string name = req.Query["name"];
+                if (data == null)
+                {
+                    return new BadRequestObjectResult("The request body could not be deserialized.");
+                }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                var card = RequestJsonToBson(data);
+                coll.InsertOne(card);
+                return new OkObjectResult(card); // Is it appropriate to respond with the object I just inserted? Seems chatty.
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "Something went wrong.");
+                return new BadRequestObjectResult("Something went wrong.");
+            }
+        }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        private static BsonDocument RequestJsonToBson(dynamic json)
+        {
+            return new BsonDocument
+            {
+                { "Title", json.Title },
+                { "Type", json.Type },
+                { "Subtype", json.Subtype },
+                { "Rarity", json.Rarity },
+                { "Cost", json.Cost },
+                { "ConvertedCost", json.ConvertedCost },
+                { "Text", json.Text },
+                { "FlavorText", json.FlavorText },
+                { "Power", json.Power },
+                { "Toughness", json.Toughness },
+                { "Expansion", new BsonDocument
+                    {
+                        { "Name", json.Expansion.Name },
+                        { "TotalCards", json.Expansion.TotalCards },
+                        { "Abbreviation", json.Expansion.Abbreviation },
+                        { "Copyright", json.Expansion.Copyright }
+                    }
+                },
+                { "CardNumber", json.CardNumber },
+                { "Artist", json.Artist }
+            };
         }
     }
 }
